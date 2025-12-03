2e27e2274997ea51e537960f162682570689d320
kwesisai <kwesiabraham4503@gmail.com>
2025-12-03 08:52:25 +0000

    expressions, statements and input with python

---
+++ Automate-with-Python/greeting.py
@@ -0,0 +1,8 @@
+#playing with the basics of python programming
+print('hey there')
+print('What is your name?')
+theName = input()
+print('Nice to meet you ' + theName)
+print ('What is your age?')
+theAge = input()
+print ('You will be ' + str(int(theAge) + 1)  + ' Next year')
